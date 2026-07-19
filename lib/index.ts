2b67a898d67a35d6345c09aa6ea1ac422a55251f
Donovan_DMC <hewwo@yiff.rocks>
2026-07-19 05:47:35 -0500

    re-export raw parser types at root
    Because of weird typescript shenanigans (TS2742), this is required to not need further shenanigans in consumers

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,16 @@ export { default as E621ExportParser } from "./E621ExportDownloader.js";
 export { default as DeferredExport } from "./DeferredExport.js";
 export { default as Export } from "./Export.js";
 export * as DefaultParsers from "./parsers/index.js";
+export type {
+    ArtistData,
+    BulkUpdateRequestData,
+    PoolData,
+    PostData,
+    PostReplacementData,
+    PostVersionData,
+    TagData,
+    TagAliasData,
+    TagImplicationData,
+    WikiPageData
+} from "./parsers/index.js";
 export type * from "./types.js";
